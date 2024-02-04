a904f6638487addc836165efd1d447bd220f340d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-02-03 19:13:30 -0600

    probably fix #1

--- src/main/java/moriyashiine/superbsteeds/mixin/HorseArmorItemMixin.java
+++ src/main/java/moriyashiine/superbsteeds/mixin/HorseArmorItemMixin.java
@@ -6,29 +6,27 @@ package moriyashiine.superbsteeds.mixin;
 
 import net.minecraft.item.HorseArmorItem;
 import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HorseArmorItem.class)
 public class HorseArmorItemMixin {
-	@Unique
-	private String name;
+	@Shadow
+	@Mutable
+	@Final
+	private int bonus;
 
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void superbsteeds$swapGoldAndIronValues(int bonus, String name, Item.Settings settings, CallbackInfo ci) {
-		this.name = name;
-	}
-
-	@Inject(method = "getBonus", at = @At("HEAD"), cancellable = true)
-	private void superbsteeds$swapGoldAndIronValues(CallbackInfoReturnable<Integer> cir) {
 		if (name.equals("iron")) {
-			cir.setReturnValue(7);
+			this.bonus = 7;
 		} else if (name.equals("gold")) {
-			cir.setReturnValue(5);
+			this.bonus = 5;
 		}
 	}
 }
