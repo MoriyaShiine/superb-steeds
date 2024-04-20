b5dc5d5ea361ccfbd2acbce4690dc28121a0b828
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-04-19 23:16:01 -0500

    mixin cleanup

--- src/main/java/moriyashiine/superbsteeds/mixin/AmbientStandGoalMixin.java
+++ src/main/java/moriyashiine/superbsteeds/mixin/AmbientStandGoalMixin.java
@@ -4,14 +4,13 @@
 
 package moriyashiine.superbsteeds.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.ai.goal.AmbientStandGoal;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AmbientStandGoal.class)
 public class AmbientStandGoalMixin {
@@ -19,10 +18,11 @@ public class AmbientStandGoalMixin {
 	@Final
 	private AbstractHorseEntity entity;
 
-	@Inject(method = "canStart", at = @At("RETURN"), cancellable = true)
-	private void superbsteeds$preventSaddleBucking(CallbackInfoReturnable<Boolean> cir) {
-		if (cir.getReturnValueZ() && entity.isSaddled()) {
-			cir.setReturnValue(false);
+	@ModifyReturnValue(method = "canStart", at = @At("RETURN"))
+	private boolean superbsteeds$preventSaddleBucking(boolean original) {
+		if (original && entity.isSaddled()) {
+			return false;
 		}
+		return original;
 	}
 }
--- src/main/java/moriyashiine/superbsteeds/mixin/PowderSnowBlockMixin.java
+++ src/main/java/moriyashiine/superbsteeds/mixin/PowderSnowBlockMixin.java
@@ -4,21 +4,21 @@
 
 package moriyashiine.superbsteeds.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.block.PowderSnowBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.HorseEntity;
 import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PowderSnowBlock.class)
 public class PowderSnowBlockMixin {
-	@Inject(method = "canWalkOnPowderSnow", at = @At("RETURN"), cancellable = true)
-	private static void superbsteeds$leatherHorseArmor(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-		if (!cir.getReturnValueZ() && entity instanceof HorseEntity horse && horse.getArmorType().isOf(Items.LEATHER_HORSE_ARMOR)) {
-			cir.setReturnValue(true);
+	@ModifyReturnValue(method = "canWalkOnPowderSnow", at = @At("RETURN"))
+	private static boolean superbsteeds$leatherHorseArmor(boolean original, Entity entity) {
+		if (!original && entity instanceof HorseEntity horse && horse.getArmorType().isOf(Items.LEATHER_HORSE_ARMOR)) {
+			return true;
 		}
+		return original;
 	}
 }
