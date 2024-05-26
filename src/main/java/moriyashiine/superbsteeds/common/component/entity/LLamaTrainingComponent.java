d5af30ff5762cb0cca9ce0b330c76b826cb76179
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-05-26 17:05:10 -0500

    update license headers

--- src/main/java/moriyashiine/superbsteeds/common/component/entity/LLamaTrainingComponent.java
+++ src/main/java/moriyashiine/superbsteeds/common/component/entity/LLamaTrainingComponent.java
@@ -1,7 +1,6 @@
 /*
- * All Rights Reserved (c) MoriyaShiine
+ * Copyright (c) MoriyaShiine. All Rights Reserved.
  */
-
 package moriyashiine.superbsteeds.common.component.entity;
 
 import moriyashiine.superbsteeds.mixin.AbstractHorseEntityAccessor;
--- src/main/java/moriyashiine/superbsteeds/mixin/CamelEntityMixin.java
+++ src/main/java/moriyashiine/superbsteeds/mixin/CamelEntityMixin.java
@@ -1,7 +1,6 @@
 /*
- * All Rights Reserved (c) MoriyaShiine
+ * Copyright (c) MoriyaShiine. All Rights Reserved.
  */
-
 package moriyashiine.superbsteeds.mixin;
 
 import net.minecraft.entity.passive.CamelEntity;
